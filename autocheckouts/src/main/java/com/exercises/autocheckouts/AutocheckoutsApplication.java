2b60f54e303de53f36d411e876f967a04f88775a
RaffaeleSaponara <112951139+RaffaeleSaponara@users.noreply.github.com>
2025-01-21 17:35:08 +0100

    Add files via upload

---
+++ autocheckouts/src/main/java/com/exercises/autocheckouts/AutocheckoutsApplication.java
@@ -0,0 +1,13 @@
+package com.exercises.autocheckouts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocheckoutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocheckoutsApplication.class, args);
+	}
+
+}
---
+++ autocheckouts/src/test/java/com/exercises/autocheckouts/AutocheckoutsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercises.autocheckouts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocheckoutsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
